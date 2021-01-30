7f184793d222c6778c42d5b157f51a4572c8fa48
João Pedro Alves <jotalmeida007@hotmail.com>
2021-01-30 11:10:48 -0300

    finalize project, and using tests

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -6,10 +6,24 @@ defmodule FizzBuzz do
       |> handle_file_read()
     end
 
-    def handle_file_read({:ok, result}) do
-      result
-      |>String.split(",")
-      |>Enum.map(&String.to_integer/1)
+    defp handle_file_read({:ok, result}) do
+      result =
+        result
+       |>String.split(",")
+       |>Enum.map(&convert_and_evaluete_numbers/1)
+
+       {:ok, result}
     end
-    def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+    defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
+
+    defp convert_and_evaluete_numbers(elem) do
+      elem
+      |> String.to_integer()
+      |>evaluete_numbers()
+    end
+
+    defp evaluete_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+    defp evaluete_numbers(number) when rem(number, 3) == 0, do: :fizz
+    defp evaluete_numbers(number) when rem(number, 5) == 0, do: :buzz
+    defp evaluete_numbers(number), do: number
 end
